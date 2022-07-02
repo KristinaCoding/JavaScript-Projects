00aa24d8bd5896975362c61fea14cefb9249100b
KristinaCoding <kristina13kovacheva@gmail.com>
2022-07-02 13:26:45 +0100

    my second commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function Shopping(){//The beginning of a function called Shopping
+var Products="Monster drink",price="2.5euro";//using variable and assigning it two values
+Products+="You can buy this product if you are 25";//creating concatenating string
+document.getElementById("shopping_list").innerHTML=Products+""+price+"";//calling id element button from HTML to display the variables in "clikc here" 
+}
+
