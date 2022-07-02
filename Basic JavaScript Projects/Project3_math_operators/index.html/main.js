63fb7e96cb46324f578a8d24c7206e541cce6d52
KristinaCoding <kristina13kovacheva@gmail.com>
2022-07-02 16:27:13 +0100

    my third commit

--- Basic JavaScript Projects/Project3_math_operators/index.html/main.js
+++ Basic JavaScript Projects/Project3_math_operators/index.html/main.js
@@ -1,48 +1,48 @@
-function Math0(){
-    var addition=8+8;
-    document.getElementById("math").innerHTML="8+8="+addition;
+function Math0(){//making a function 
+    var addition=8+8;//adding numbers
+    document.getElementById("math").innerHTML="8+8="+addition;//making sure it is display and responsive
 }
 
-function Math1(){
-    var subtraction=8-4;
-    document.getElementById("math1").innerHTML="8-4="+subtraction;
+function Math1(){//making function
+    var subtraction=8-4;//subtracting numbers
+    document.getElementById("math1").innerHTML="8-4="+subtraction;//making sure it is display and responsive
 }
 
-function Math2(){
-    var multiply=8*8;
-    document.getElementById("math2").innerHTML="8*8="+multiply;
+function Math2(){//making function
+    var multiply=8*8;//multiplying the numbers
+    document.getElementById("math2").innerHTML="8*8="+multiply;//making sure it is display and responsive
 }
 
-function Math3(){
-    var divide=64/8;
-    document.getElementById("math3").innerHTML="64/8="+divide;
+function Math3(){//making function
+    var divide=64/8; //divding the numebrs 
+    document.getElementById("math3").innerHTML="64/8="+divide;//making sure it is display and responsive
 }
 
-function Math4(){
-    var math4=(8+8)*10/2-5;
-    document.getElementById("math4").innerHTML="8+8*10/2-5=" + math4;
+function Math4(){//making function
+    var math4=(8+8)*10/2-5; //muptiple math operation of the given numbers
+    document.getElementById("math4").innerHTML="8+8*10/2-5=" + math4;//making sure it is display and responsive
 }
 
-function Math5(){
-    var modulus=64%9;
-    document.getElementById("math5").innerHTML="64%9=" + modulus;
+function Math5(){//making function
+    var modulus=64%9;//modulus operation showing the remainder
+    document.getElementById("math5").innerHTML="64%9=" + modulus;//making sure it is display and responsive
 }
 
-function Math6(){
-    var y=55;
-    document.getElementById("math6").innerHTML= -y;
+function Math6(){//making function
+    var y=55;//showing the opposite /negative number
+    document.getElementById("math6").innerHTML= -y;//making sure it is display and responsive
     }
 
-    var y=88;
-    y++;
-    document.write(y);
+    var y=88;//creating a variable
+    y++;//varibale increment
+    document.write(y);//making sure it is displayed
     
-    var y=88;
-    y--;
-    document.write(y);
+    var y=88;//creating a variable
+    y--;//variable decrementtttttt
+    document.write(y);//making sure it is displayeeeedd
 
-    window.alert(Math.random()*25);
+    window.alert(Math.random()*25);//pop-up window appearing with random number
 
    
-   document.write(Math.pow(4,2));
+   document.write(Math.pow(4,2));//put number in power 4*4 in this case
   
