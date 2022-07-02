396129d4526f1566540758c3f781a54dd01dc1e1
KristinaCoding <kristina13kovacheva@gmail.com>
2022-07-02 16:16:41 +0100

    my third commit

---
+++ Basic JavaScript Projects/Project3_math_operators/index.html/main.js
@@ -0,0 +1,48 @@
+function Math0(){
+    var addition=8+8;
+    document.getElementById("math").innerHTML="8+8="+addition;
+}
+
+function Math1(){
+    var subtraction=8-4;
+    document.getElementById("math1").innerHTML="8-4="+subtraction;
+}
+
+function Math2(){
+    var multiply=8*8;
+    document.getElementById("math2").innerHTML="8*8="+multiply;
+}
+
+function Math3(){
+    var divide=64/8;
+    document.getElementById("math3").innerHTML="64/8="+divide;
+}
+
+function Math4(){
+    var math4=(8+8)*10/2-5;
+    document.getElementById("math4").innerHTML="8+8*10/2-5=" + math4;
+}
+
+function Math5(){
+    var modulus=64%9;
+    document.getElementById("math5").innerHTML="64%9=" + modulus;
+}
+
+function Math6(){
+    var y=55;
+    document.getElementById("math6").innerHTML= -y;
+    }
+
+    var y=88;
+    y++;
+    document.write(y);
+    
+    var y=88;
+    y--;
+    document.write(y);
+
+    window.alert(Math.random()*25);
+
+   
+   document.write(Math.pow(4,2));
+  
