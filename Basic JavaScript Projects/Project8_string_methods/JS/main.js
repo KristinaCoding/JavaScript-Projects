89b2c234d7b753afc09fb052b88e92d5eddddf8a
KristinaCoding <kristina13kovacheva@gmail.com>
2022-07-04 12:12:32 +0100

    my 8th commit

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,23 +1,23 @@
-function My_Function(){
-var part_1="I"
+function My_Function(){//calling a function
+var part_1="I"//creating var=variables
 var part_2="love"
 var part_3="dancing"
-var whole_sentence=part_1.concat(part_2,part_3);
-document.getElementById("Conc1").innerHTML = whole_sentence;
+var whole_sentence=part_1.concat(part_2,part_3);//getting all variables concatenated
+document.getElementById("Conc1").innerHTML = whole_sentence;//displaying text
 }
 
-function Slice_Function(){
-var Sentence="All things happen for a reason!";
-var Section=Sentence.slice(10,46);
-document.getElementById("Slice").innerHTML=Section;
+function Slice_Function(){//calling a function
+var Sentence="All things happen for a reason!";//creating a sentence
+var Section=Sentence.slice(10,46);//using slice methos
+document.getElementById("Slice").innerHTML=Section;//displaying the "happen for reason" part
 }
 
-function My_Function1(){
-var x=13;
-document.getElementById("toString1").innerHTML=x.toString();
+function My_Function1(){//calling function
+var x=13;//creating a variable
+document.getElementById("toString1").innerHTML=x.toString();//using toString to dispay text
 }
 
-function My_Function2(){
-    var y=131255.30115678
-    document.getElementById("precision2").innerHTML=y.toPrecision(5);
+function My_Function2(){//calling function
+    var y=131255.30115678//creating a variable
+    document.getElementById("precision2").innerHTML=y.toPrecision(5);//using precision method
 }
