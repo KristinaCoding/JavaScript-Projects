3c2e66b5dd8f181167e05f3081c3bd915e1a5717
KristinaCoding <kristina13kovacheva@gmail.com>
2022-07-04 09:34:59 +0100

    new commit

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,15 +2,9 @@ window.alert("Hello,World!"); //It shows pop-up page where Hello World is displa
 
 document.write("Hello,World!"); //It shows on the page itself  Hello,World!
 
-var A="This is a string!"; //I created a variable with a string that displays on the main page
-
-document.write(A);//this is making sure the variable A is displayed on the screen
-
-var Y= ("I said:\"I will do it myself\"and ended the conversation."); //the backslash makes the quotations appear in the main page
-
-document.write(Y);//this is making sure the variable Y is displayed on the screen
-
-document.write('Hello' + ' ' + 'World');//here +c means concatenating the words
+var A="Kristina"//vriable 1
+var B="13";//variable 2
+document.write(A+B);//concatenating variable
 
 var pinks=("Pink is one of my favourite colors");//this variable pinks has text
 var pinks=pinks.fontcolor("pink");//the text above will become pink
